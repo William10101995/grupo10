e4cfbe8d7e14e22c0f25dfd0980dd36b1334a20b
humberbogado1602 <humber16.02@gmail.com>
2021-11-21 20:32:13 -0300

    Agregado las variables necesarias e identificada las innecesarias

--- variables.tf
+++ variables.tf
@@ -2,6 +2,33 @@ variable "region" {
   description = "Region in AWS"
 }
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type = string
+}
+
+variable "ami_id" {
+  description = "AMI id"
+  type = string
+}
+
+variable "vpc1_id" {
+  description = "VPC id"
+  type = string
+}
+
+variable "internet_gateway_id" {
+  description = "Gateway id"
+  type = string
+}
+
+variable "key1" {
+  description = "KEY"
+  type = string
+}
+
+#las variables de abajo no están en el repo de guía, por lo que no se utilizarán
+
 variable "vpc_cidr" {
   description = "VPC Main 1 CIDR"
 }
@@ -13,27 +40,13 @@ variable "subnet1_cidr" {
 variable "subnet1_zone_1a" {
   description = "Zone 1a - Subnet 1"
 }
-variable "ami_id" {
-  description = "AMI id"
-  type = string
-}
 
 variable "subnet1_private_ip" {
   description = "Subnet 1 Private IP"
   type = string
 }
 
-variable "key1" {
-  description = "KEY"
-  type = string
-}
-
 variable "username" {
   description = "Resource Owner | AWS IAM username"
-  type = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
   type = string
 }
