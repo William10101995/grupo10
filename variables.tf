6746a3e283d5edfbe24c79cb436ccdd23595dd67
humberbogado1602 <humber16.02@gmail.com>
2021-11-21 21:27:11 -0300

    grupo10-v1.0

--- variables.tf
+++ variables.tf
@@ -26,27 +26,3 @@ variable "key1" {
   description = "KEY"
   type = string
 }
-
-#las variables de abajo no están en el repo de guía, por lo que no se utilizarán
-
-variable "vpc_cidr" {
-  description = "VPC Main 1 CIDR"
-}
-
-variable "subnet1_cidr" {
-  description = "Subnet 1 CIDR"
-}
-
-variable "subnet1_zone_1a" {
-  description = "Zone 1a - Subnet 1"
-}
-
-variable "subnet1_private_ip" {
-  description = "Subnet 1 Private IP"
-  type = string
-}
-
-variable "username" {
-  description = "Resource Owner | AWS IAM username"
-  type = string
-}
